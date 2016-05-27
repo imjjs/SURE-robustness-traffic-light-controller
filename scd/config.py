3a7c36643501a15d852a15af0f4fcde5e97c679a
imjjs <liyizhe20@gmail.com>
2016-05-26 21:11:49 -0500

    aa

---
+++ scd/config.py
@@ -0,0 +1,88 @@
+import os
+from threading import Lock
+import socket
+import intersection
+
+
+PORT_LOCK = Lock()
+
+
+def get_open_port(howMany=1):
+    """Return a list of n free port numbers on localhost"""
+    results = []
+    sockets = []
+    i=0
+    while i < howMany:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('localhost', 0))
+        # work out what the actual port number it's bound to is
+        addr, port = s.getsockname()
+        if port < 40000:
+            s.close()
+            continue
+        results.append(port)
+        sockets.append(s)
+        i += 1
+    for s in sockets:
+        s.close()
+
+    return results
+
+def generator_ports():
+    PORT_LOCK.acquire()
+    ports = get_open_port(1)
+    PORT_LOCK.release()
+    return ports[0]
+
+
+def generator_intersectionList(intersection_names, paraList):
+    ret = []
+    for name in intersection_names:
+        ins = intersection.Intersection(name)
+        ins.loadFromData(IN_DATA)
+        ret.append(ins)
+    assert len(ret) == len(paraList)
+
+    for idx in range(len(ret)):
+        ret[idx].setThreshold(paraList[idx][0], paraList[idx][1])
+    return ret
+
+sumoMaps = [
+    os.path.join('..','VanderbiltCampus','Vanderbilt.sumo.cfg')
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (1).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (2).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (3).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (4).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (5).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (6).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (7).cfg'),
+    # os.path.join('VanderbiltCampus','Vanderbilt.sumo (8).cfg'),
+ #   os.path.join('VanderbiltCampus','Vanderbilt.sumo (9).cfg'),
+ #   os.path.join('VanderbiltCampus','Vanderbilt.sumo (10).cfg'),
+]
+
+
+# NS_GREEN, WE_GREEN, w, e, n, s
+# WE_RED, NS_Red
+IN_DATA = {
+    "1443088101": ["GGGggrrrrGGGggrrrr", "rrrrrGGggrrrrrGGgg", 3, 2, 2, 2],#
+    "202270699": ["GGGGgrrrGGGggrrr", "rrrrrGGgrrrrrGgg", 1, 2, 3, 3],
+    "202514063": ["GGGGggrrrrGGGGggrrrr", "rrrrrrGGggrrrrrrGGgg", 2, 1, 3, 3],#
+    "202305800": ["GGGGggrrrrGGGGggrrrr", "rrrrrrGGggrrrrrrGGgg", 1, 1, 3, 3],#
+    "202514074": ["GGGrrrrGgGrrrr", "rrGGGGgrrGGGGg", 3, 3, 2, 2],#
+    "202514078": ["GGGGggrrrrGGGGggrrrr", "rrrrrrGGggrrrrrrGGgg", 1, 2, 3, 3],#
+    "3010263944": ["GGggrrrrrGGggrrrrr", "rrrrGGGggrrrrGGGgg", 2, 2, 2, 2],#
+    "1443088096": ["GGGGggrrGGGGggrrrr", "GrrrrrGGrrrrrrGGgg", 1, 2, 3, 3],#
+    "202407913": ["GGGggGGGGgrrrr", "rrrrrrrrrrGGGG", 0, 2, 3, 3],#
+    "IK" :["Grr", "rGG", 2, 0, 1, 0],
+    "LJ" :["Grr", "rGG", 2, 0, 1, 0],
+    "GD" :["Grr", "rGG", 2, 0, 1, 0],
+    "FH" :["Grr", "rGG", 2, 0, 1, 0],
+    "AC" :["GGrr", "rrGG", 2, 0, 2, 0],
+}
+
+smallMap = ['IK', 'LJ', 'GD', 'FH', 'AC']
+CompareList = ['1443088096','1443088101','202514078','3010263944','202407913']
+IntersectionList2 = ["202514063", "202305800", "202514074", "202514078", "3010263944", "1443088101", "202270699"]
+IntersectionList = ['1443088096', '1443088101', '202514078', '3010263944', '202407913', '202514074', '202305800', '202514063', '202270699']
+DEVNULL = open(os.devnull, "w")
---
+++ scd/main.py
@@ -0,0 +1,99 @@
+import sys
+
+
+import multiprocessing
+import Queue
+from intersection import Intersection
+import config
+import test
+import time
+import socket
+import log
+import random
+
+port_que = Queue.Queue()
+
+
+
+INPUT_INTERSECTION = config.CompareList
+
+
+
+CoreNumber = 16
+
+TestPeriod = 36000
+testRange = (2, 30)
+stepLength = 2
+
+def mytestWarp(tup):
+    speed, we, ns = test.mytest(tup[0], tup[1], tup[2], tup[3], tup[4], tup[5])
+    return (speed, we, ns,)
+
+
+
+
+
+def start_process():
+    pass
+
+if __name__ == '__main__':
+    log.LogTime = time.time()
+    with open('logtime','w') as f:
+        f.write(str(log.LogTime))
+    jobs = []
+    paraList = []
+    for ele in INPUT_INTERSECTION:
+        paraList.append((0,0,))
+
+
+    # fix_config = [1500,1100,1100,700,1100]
+    # for idx in range(len(intersections)):
+    #     intersections[idx].lightMax = max([fix_config[idx], 1800 - fix_config[idx]])
+    #     intersections[idx].lightMin = min([fix_config[idx], 1800 - fix_config[idx]])
+
+#    mytest(10,10, intersections,1,41000)
+    idx = 0
+    #for idx in range(len(paraList)):
+    while True:
+	print idx
+        if time.time() - log.LogTime > TestPeriod:
+            break
+        pool = multiprocessing.Pool(processes = CoreNumber,
+                                initializer = start_process)
+        inputList = []
+        inputList.append((0, 0, paraList, INPUT_INTERSECTION, idx, config.sumoMaps))
+
+        for i in range(testRange[0], testRange[1], stepLength):
+            for j in range(testRange[0], testRange[1], stepLength):
+                inputList.append((i, j, paraList, INPUT_INTERSECTION, idx, config.sumoMaps))
+                #procID += 1
+
+
+
+
+        result = pool.map(mytestWarp, inputList)
+        #result = map(mytestWarp, inputList)
+        pool.close()
+        pool.join()
+
+        f = open("intersection" + str(idx) + ".txt", "w")
+        for i in result:
+            #print i[0], i[1], i[2]
+            f.write(str(i[0]) +str(i[1]) + str(i[2]) + '\n')
+
+
+
+        maxSpeed, minWeThreshold, minNsThreshold = max(result, key = lambda x: x[0])
+        f.write("final:"+ str(maxSpeed) + ',' + str(minWeThreshold) + ',' + str(minNsThreshold))
+
+        #minDuration, minWeThreshold, minNsThreshold = min(result, key = lambda x: x[0])
+        #f.write("final:"+ str(minDuration) + ',' + str(minWeThreshold) + ',' + str(minNsThreshold))
+
+        paraList[idx] = (minWeThreshold, minNsThreshold,)
+        f.flush()
+        idx = (idx + 1) % len(paraList)
+        time.sleep(10)
+
+        print "sleeping at loot--------"
+
+    print paraList
